43a3ccda7556d5a41942b94814ea2019b02e3f8f
Diyary311 <arazomar500@gmail.com>
2025-03-07 22:56:43 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,11 +40,7 @@ INSTALLED_APPS = [
     'homepage',
 ]
 
-STATIC_URL = "static/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'homepage', 'static'),  # Optional global static directory
-]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
